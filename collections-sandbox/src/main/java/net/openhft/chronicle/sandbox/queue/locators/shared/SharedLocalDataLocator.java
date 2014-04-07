bbb898438d7ba7f02d5386f780a9dbcfdd6b8754
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2014-04-07 09:29:08 +0100

    Code tidy

--- collections-sandbox/src/main/java/net/openhft/chronicle/sandbox/queue/locators/shared/SharedLocalDataLocator.java
+++ collections-sandbox/src/main/java/net/openhft/chronicle/sandbox/queue/locators/shared/SharedLocalDataLocator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Peter Lawrey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.openhft.chronicle.sandbox.queue.locators.shared;
 
 import net.openhft.chronicle.sandbox.queue.locators.DataLocator;
@@ -53,7 +69,7 @@ public class SharedLocalDataLocator<E> implements DataLocator<E> {
 
             storeSlice.busyLockLong(offset);
             storeSlice.position(offset + LOCK_SIZE);
-            return (E) storeSlice.readInstance((Class) valueClass, null);
+            return storeSlice.readInstance(valueClass, null);
 
         } catch (InterruptedException e) {
             e.printStackTrace();
